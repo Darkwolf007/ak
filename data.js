812bdee033d9809e1324467df234bcaf14981666
Darkwolf007 <31916350+Darkwolf007@users.noreply.github.com>
2021-09-29 23:24:02 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,336 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-living360a",
+      "name": "Living360A",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5872017186023264,
+          "pitch": 0.2820598495114268,
+          "rotation": 0,
+          "target": "1-living360b"
+        },
+        {
+          "yaw": -2.5726220389294934,
+          "pitch": 0.041465181932691664,
+          "rotation": 0,
+          "target": "8-kitchen360a"
+        },
+        {
+          "yaw": -1.8985387928952093,
+          "pitch": 0.11219267028246449,
+          "rotation": 0,
+          "target": "2-bedroom03360b"
+        },
+        {
+          "yaw": -1.6750397779243986,
+          "pitch": 0.08574391057225839,
+          "rotation": 0,
+          "target": "6-master-bedroom360a"
+        },
+        {
+          "yaw": -1.5102757912727807,
+          "pitch": 0.1969331637489944,
+          "rotation": 0,
+          "target": "3-bedroom04360a"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-living360b",
+      "name": "Living360B",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4463069432477518,
+          "pitch": 0.1240737840584476,
+          "rotation": 0,
+          "target": "9-terrace360a"
+        },
+        {
+          "yaw": 1.6273126594014906,
+          "pitch": 0.027082020091917514,
+          "rotation": 0,
+          "target": "8-kitchen360a"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-bedroom03360b",
+      "name": "Bedroom03360B",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4542539407345982,
+          "pitch": 0.20858747877140082,
+          "rotation": 0,
+          "target": "5-bedroom03360a"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-bedroom04360a",
+      "name": "Bedroom04360A",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6507965419769679,
+          "pitch": 0.24170080739809308,
+          "rotation": 0,
+          "target": "4-bedroom04360b"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-bedroom04360b",
+      "name": "Bedroom04360B",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7008957348691425,
+          "pitch": 0.16810921061465223,
+          "rotation": 0,
+          "target": "0-living360a"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-bedroom03360a",
+      "name": "Bedroom03360A",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-master-bedroom360a",
+      "name": "Master Bedroom360A",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6806048936298019,
+          "pitch": 0.16120678830868762,
+          "rotation": 0,
+          "target": "7-master-bedroom360b"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-master-bedroom360b",
+      "name": "Master Bedroom360B",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-kitchen360a",
+      "name": "Kitchen360A",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-terrace360a",
+      "name": "Terrace360A",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-terrace360b",
+      "name": "Terrace360B",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
